cf43cf0a2e8a87e268361f67f2a4c543fc896281
agus_artero <agusartero7@gmail.com>
2022-08-23 18:14:58 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+let fecha = Number (prompt("Ingrese su año de nacimiento"));
+const año = 2022;
+
+let edad = año - fecha;
+
+alert("Su edad es aproximada a"  + " " + edad); */
+
+
+let nota = Number(prompt("Ingrese su nota"));
+
+    if (nota > 10) { 
+        alert("La nota no puede ser mayor a 10.") ; 
+    }
+        if (nota <= 6)
+            alert("TU NOTA ES DESAPROBADA");
+        else {
+            alert("TU NOTA ES APROBADA");
+}
+
+
+    /* 
+for (let i = 0; i <= 40; i++) {
+    console.log(i);
+    i = i + 1;
+} */
